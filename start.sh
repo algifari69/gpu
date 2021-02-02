67a9b1a7b2a630d4f3beb0f43836e443998717a3
feritys102 <78082710+feritys102@users.noreply.github.com>
2021-02-02 08:29:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-while true
-do
+chmod +x wildrig-multi
+
 ./wildrig-multi --print-full --algo megamec --url stratum+tcp://megamec.mine.zpool.ca:3560 --user DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF --pass gpu
-sleep 5
+
 done
