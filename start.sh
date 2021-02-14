394e3e3d290af7f1e747329834867d3a97fd78a7
feritys102 <78082710+feritys102@users.noreply.github.com>
2021-02-14 14:15:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 chmod +x nbminer
 
-./nbminer -a ethash -o stratum+tcp://ethash.mine.zergpool.com:9999 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -p c=DOGE
+./nbminer -a ethash -o stratum+tcp://ethash.mine.zergpool.com:9999 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF.c=DOGE -log
 
 done
